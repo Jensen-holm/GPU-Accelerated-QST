b2eb47d65f60308fd0c5c2e217922fac553a0149
Jensen-holm <holmj@mail.gvsu.edu>
2023-12-23 11:25:01 -0500

    updating gitignore

--- tests/test.out[1]_tests.cmake
+++
@@ -1,3 +0,0 @@
-add_test([=[simpleTest.Equality]=]  /Users/jensen/Documents/projects/Undergraduate-Research/tests/test.out [==[--gtest_filter=simpleTest.Equality]==] --gtest_also_run_disabled_tests)
-set_tests_properties([=[simpleTest.Equality]=]  PROPERTIES WORKING_DIRECTORY /Users/jensen/Documents/projects/Undergraduate-Research/tests SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-set(  test.out_TESTS simpleTest.Equality)
